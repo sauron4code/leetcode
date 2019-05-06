821156374ffadbc86acb4d73b654eab5220bd890
黎海明 <laughing@jollycorp.com>
2019-05-06 22:52:39 +0800

    添加题目描述

--- src/main/java/L234.java
+++ src/main/java/L234.java
@@ -5,7 +5,24 @@ import java.util.List;
  * @lc app=leetcode.cn id=234 lang=java
  *
  * [234] 回文链表
+ *
+回文链表
+
+请判断一个链表是否为回文链表。
+
+示例 1:
+
+输入: 1->2
+输出: false
+示例 2:
+
+输入: 1->2->2->1
+输出: true
+进阶：
+你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
  */
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
